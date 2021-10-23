ecbee692cdf19411e8ae6cc177df5baa4aa07b34
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-10-23 16:51:56 -0400

    added additional routes and config

--- routes/note-routes.js
+++ routes/note-routes.js
@@ -1,6 +1,27 @@
 const router = require('express').Router();
+const { notes } = require('../db/db.json');
 
+router.get('/api/notes', (req, res) => {
+    let results = notes;
+    if(req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
 
+router.get('/api/notes/:id', (req, res) => {
+    const result = findbyId(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
 
+router.post('/api/notes', (req, res) => {
+    req.body = notes.length.toString();
+    const note = saveNote(req.body, notes); 
+    res.json(note);
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
 const express = require('express');
+const noteRoutes = require('./routes/note-routes');
+const htmlRoutes = require('./routes/htmlRoutes/index.js');
+
+const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
