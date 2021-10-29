8d4fe126364e98cdbc8edca625a6bd68069adeed
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-10-29 18:43:42 -0400

    got routes working

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const noteRoutes = require('./note-routes');
+router.use(require('./htmlRoutes/index'))
 router.use(noteRoutes);
 
 module.exports = router;
--- routes/note-routes.js
+++ routes/note-routes.js
@@ -1,15 +1,19 @@
 const router = require('express').Router();
 const { notes } = require('../db/db.json');
+const path = require('path');
+const fs = require('fs');
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     let results = notes;
-    if(req.query) {
-        results = filterByQuery(req.query, results);
-    }
+    // if(req.query) {
+    //     results = filterByQuery(req.query, results);
+    // }
+    //res.sendFile(path.join(__dirname, "../db/db.json"));
+    console.log("db.content", notes);
     res.json(results);
 });
 
-router.get('/api/notes/:id', (req, res) => {
+router.get('/notes/:id', (req, res) => {
     const result = findbyId(req.params.id, notes);
     if (result) {
         res.json(result);
@@ -18,7 +22,7 @@ router.get('/api/notes/:id', (req, res) => {
     }
 });
 
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     req.body = notes.length.toString();
     const note = saveNote(req.body, notes); 
     res.json(note);
